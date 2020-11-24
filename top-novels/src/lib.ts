396fe9a4e1144a69104f740430589adc4bcee052
youyiqin <564151947@qq.com>
2020-11-25 02:13:24 +0800

    修改逻辑，降低速度，增加重试机制

--- top-novels/src/lib.ts
+++ top-novels/src/lib.ts
@@ -1,63 +1,76 @@
+import { rejects } from 'assert'
 import axios from 'axios'
 const cheerio = require('cheerio')
+const chunk = require('lodash/chunk')
 const fs = require('fs')
-const fsPromises = fs.promises
 const path = require('path')
 const iconv = require("iconv-lite");
 export const basicUrl = 'https://www.230book.com/'
 
-export default async function download( bookId:string ) {
-  const homeUrl = `${basicUrl}book/${bookId}`
+export default async function download( bookid:string ) {
+  const homeUrl = `${basicUrl}book/${bookid}`
   const homepageContent = await axios.get(homeUrl, {
     responseType: "arraybuffer"
   })
   // 转码中文
   let $ = cheerio.load(iconv.decode(homepageContent.data, "gb2312"))
   const bookName = $('#info h1').text()
-  let chapterPromiseList: any = [];
-  // console.log($('#list ._chapter li'));
-  const itemArr = $('#list ._chapter li')
-  itemArr.find('a').each(async function (i: number, e: any) {
-    // console.log($(e).attr('href'));
-    if (i < 1) {
-      const chapterUrl = `${homeUrl}/${$(e).attr('href')}`
-      chapterPromiseList.push(await downloadChapterContent(chapterUrl, i))
-    }
+  const chapterUrlList: string[] = []
+  $('#list ._chapter li').each((i: number, elem: HTMLElement) => {
+    const chapterUrl = `${homeUrl}/${$('a', elem).attr('href')}`
+    chapterUrlList.push(chapterUrl)
+  })
+  console.log('已获取章节，总数：', chapterUrlList.length, '，开始准备下载：');
+  // 过滤掉一些莫名其妙的章节，顶点的末章是空的
+  const chunkChapterUrlList = chunk(chapterUrlList.filter(item => !/undefined$/.test(item)), 10)
+  let lastResultPromiseArr:any = chunkChapterUrlList.map((item: string[], index: number) => {
+    return new Promise((resolve, rejects) => {
+      const chapterPromiseArr = item.map(async (url) => {
+        return await downloadChapterContent(url)
+      })
+      Promise.all(chapterPromiseArr).then(result => {
+        console.log('Waiting...');
+        resolve(result.reduce((prev, next) => prev + next))
+      })
+    })
   })
 
-  Promise.all(chapterPromiseList).then(chapter => {
-    console.log(chapter);
+  Promise.all(lastResultPromiseArr).then((result: string[]) => {
+    const content = result.reduce((prev, next) => prev + next)
+    console.log(content);
+
   })
+
 }
 
-const downloadChapterContent = async (url: string, chapterIndex: number) => {
-  return new Promise(async (resolve, rejects) => {
-    console.log('downloading。。。', url);
 
+const downloadChapterContent = async (url: string, retryTimes = 3): Promise<string> => {
+  return new Promise(async (resolve, rejects) => {
     axios.get(url, {
-      responseType: 'arraybuffer'
+      responseType: 'arraybuffer',
+      headers: {
+        "user-agent": 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36'
+      }
     }).then(async (resp) => {
       if (resp.status !== 200) rejects(url)
       const $ = cheerio.load(iconv.decode(resp.data, "gb2312"))
       const title = $('.bookname h1').text()
-      const content = $('#content').text().replace("&nbsp;&nbsp;&nbsp;&nbsp;", "\t")
-
-      resolve(await save(`\t\t\t\t${title}\n\n${content}\n`, path.join(process.cwd(), 'download', `${chapterIndex}.txt`)))
+      console.log('已下载：', title);
+      const text = $('#content')
+        .text()
+        .replace(/\s{4}/g, '\t\t\n\n\t\t')
+        .replace(/作者君用心创作，无奈订阅低迷，深夜码字，须养家糊口，起点正版订阅支持！/g, '')
+        .replace(/【正版订阅的同学，五分钟后刷新即可！】/g, '')
+      resolve(`\t\t${title}`)
     }).catch(err => {
-      console.log('下载失败', err.message);
-      rejects(url)
+      if (retryTimes >= 0) {
+        console.log('下载失败', err.message, url,'重试中...');
+        retryTimes -= 1
+        downloadChapterContent(url, retryTimes)
+      } else {
+        console.log(`${url}, Error: ${err.message},无法下载，跳过此章节。`);
+        resolve(`\n\n\t\t${url}\t\t\n\n`)
+      }
     })
-  })
-}
-
-const save = async (content: string, savePath = path.join(process.cwd(), 'download','novel.txt')) => {
-  return new Promise(async (resolve) => {
-    try {
-      await fsPromises.writeFile(savePath, content, 'utf8')
-      resolve(true)
-    } catch (error) {
-      console.log(error.message);
-      resolve(false)
-    }
   })
 }
