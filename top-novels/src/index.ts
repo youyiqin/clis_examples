78f947668d8b1b24df3652091f6d3958f2556dde
youyiqin <564151947@qq.com>
2020-11-26 02:16:52 +0800

    降低速度，这速度基本上是不会出错了，添加搜索机制。完成开发。

--- top-novels/src/index.ts
+++ top-novels/src/index.ts
@@ -1,5 +1,11 @@
 import {Command, flags} from '@oclif/command'
+import { result } from 'lodash';
 import download from './lib'
+const axios = require('axios')
+const cheerio = require('cheerio')
+const iconv = require("iconv-lite");
+const { encodeGBK } = require('gbk-string');
+const inquirer = require('inquirer')
 
 class TopNovels extends Command {
   static description = "顶点小说下载工具"
@@ -21,6 +27,39 @@ class TopNovels extends Command {
       await download(bookId)
     } else {
       console.log('search: ', name);
+      axios.post(`https://www.230book.com/modules/article/search.php?searchkey=${encodeGBK(name)}&searchtype=articlename`, {}, {
+        responseType: "arraybuffer"
+      }).then((resp: any) => {
+        // 转码中文
+        let $ = cheerio.load(iconv.decode(resp.data, "gb2312"))
+        let resultArr: {name: string, url: string}[] = []
+        $('#nr').each((i: number, elem: HTMLElement) => {
+          const name = $('td a', elem).first().text()
+          const url = $('td a', elem).first().attr('href')
+          resultArr.push({
+            name, url
+          })
+        })
+        if (resultArr.length === 0) {
+          console.log('无搜索结果，再次搜索则需要等待30秒。');
+        }
+        inquirer.prompt([
+          {
+            type: 'list',
+            name: 'url',
+            message: '搜索结果选择',
+            choices: resultArr.map((item: { name: string, url: string }) => {
+              return {
+                name: item.name,
+                value: item.url
+              }
+            })
+          }
+        ]).then(async (answer: any) => {
+          // 选择了下载目标，执行下载
+          await download(answer.url.replace('https://www.230book.com/book/', '').replace('/', ''))
+        })
+      })
     }
   }
 }
--- top-novels/src/lib.ts
+++ top-novels/src/lib.ts
@@ -1,11 +1,17 @@
-import { rejects } from 'assert'
 import axios from 'axios'
 const cheerio = require('cheerio')
-const chunk = require('lodash/chunk')
+const iconv = require("iconv-lite");
 const fs = require('fs')
 const path = require('path')
-const iconv = require("iconv-lite");
+const cliProgress = require('cli-progress')
+const myProgressBar = new cliProgress.SingleBar({
+  hideCursor: true,
+  format: '进度：| {bar} | {value}/{total}, 当前下载章节：{title}',
+  barCompleteChar: '\u2588',
+  barIncompleteChar: '\u2591',
+})
 export const basicUrl = 'https://www.230book.com/'
+
 
 export default async function download( bookid:string ) {
   const homeUrl = `${basicUrl}book/${bookid}`
@@ -15,35 +21,42 @@ export default async function download( bookid:string ) {
   // 转码中文
   let $ = cheerio.load(iconv.decode(homepageContent.data, "gb2312"))
   const bookName = $('#info h1').text()
-  const chapterUrlList: string[] = []
-  $('#list ._chapter li').each((i: number, elem: HTMLElement) => {
-    const chapterUrl = `${homeUrl}/${$('a', elem).attr('href')}`
-    chapterUrlList.push(chapterUrl)
-  })
-  console.log('已获取章节，总数：', chapterUrlList.length, '，开始准备下载：');
-  // 过滤掉一些莫名其妙的章节，顶点的末章是空的
-  const chunkChapterUrlList = chunk(chapterUrlList.filter(item => !/undefined$/.test(item)), 10)
-  let lastResultPromiseArr:any = chunkChapterUrlList.map((item: string[], index: number) => {
-    return new Promise((resolve, rejects) => {
-      const chapterPromiseArr = item.map(async (url) => {
-        return await downloadChapterContent(url)
-      })
-      Promise.all(chapterPromiseArr).then(result => {
-        console.log('Waiting...');
-        resolve(result.reduce((prev, next) => prev + next))
+  const chapterUrlList: {title: string, url: string}[] = []
+  $('#list ._chapter li a')
+    .each((i: number, elem: HTMLElement) => {
+      const chapterUrl = `${homeUrl}/${$(elem).attr('href')}`
+      chapterUrlList.push({
+        title: $(elem).text(),
+        url: chapterUrl
       })
     })
+  console.log('已获取章节，总数：', chapterUrlList.length, '，开始准备下载：');
+  let txtArr = []
+  let index = 0
+  while (index < chapterUrlList.length) {
+    const { url, title } = chapterUrlList[index]
+    const content = await downloadChapterContent(url)
+    // 逐一下载
+    txtArr.push(content)
+    myProgressBar.start(chapterUrlList.length, index, {title: ''})
+    index += 1
+    myProgressBar.update(index, {title})
+  }
+  myProgressBar.update(index)
+  myProgressBar.stop()
+  // 修复
+  txtArr.map(async (item) => {
+    const savePath = path.join(process.cwd(), `${bookName}.txt`)
+    if (item.startsWith('https')) {
+      const result = await downloadChapterContent(item.trim(), 0)
+      fs.appendFileSync(savePath, result, 'utf8')
+    } else {
+      fs.appendFileSync(savePath, item, 'utf8')
+    }
   })
-
-  Promise.all(lastResultPromiseArr).then((result: string[]) => {
-    const content = result.reduce((prev, next) => prev + next)
-    console.log(content);
-
-  })
-
 }
 
-
+// 返回字符串
 const downloadChapterContent = async (url: string, retryTimes = 3): Promise<string> => {
   return new Promise(async (resolve, rejects) => {
     axios.get(url, {
@@ -52,24 +65,27 @@ const downloadChapterContent = async (url: string, retryTimes = 3): Promise<stri
         "user-agent": 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36'
       }
     }).then(async (resp) => {
-      if (resp.status !== 200) rejects(url)
+      // 异常响应，写入地址，后期修复替换。
+      if (resp.status !== 200) resolve(`\t\t${url}\n\n`)
+      // 正常下载
       const $ = cheerio.load(iconv.decode(resp.data, "gb2312"))
       const title = $('.bookname h1').text()
-      console.log('已下载：', title);
       const text = $('#content')
         .text()
         .replace(/\s{4}/g, '\t\t\n\n\t\t')
         .replace(/作者君用心创作，无奈订阅低迷，深夜码字，须养家糊口，起点正版订阅支持！/g, '')
         .replace(/【正版订阅的同学，五分钟后刷新即可！】/g, '')
-      resolve(`\t\t${title}`)
+      resolve(`\t\t${title}\n\n${text}\n\n\n\n`)
     }).catch(err => {
-      if (retryTimes >= 0) {
+      // 三次重试
+      if (retryTimes > 0) {
         console.log('下载失败', err.message, url,'重试中...');
         retryTimes -= 1
         downloadChapterContent(url, retryTimes)
       } else {
-        console.log(`${url}, Error: ${err.message},无法下载，跳过此章节。`);
-        resolve(`\n\n\t\t${url}\t\t\n\n`)
+        // 先行跳过
+        console.log(`${url}, Error: ${err.message},暂时跳过此章节，后续修复补全丢失内容,修复失败则直接写入地址。`);
+        resolve(`\t\t${url}\n\n`)
       }
     })
   })
