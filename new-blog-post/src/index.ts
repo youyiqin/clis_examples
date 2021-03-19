22e5a86b7052f66aa17943b610aec3299a40ab78
youyiqin <564151947@qq.com>
2021-03-19 22:00:12 +0800

    fix: eslint rules

--- new-blog-post/src/index.ts
+++ new-blog-post/src/index.ts
@@ -1,46 +1,56 @@
-import { Command, flags } from "@oclif/command";
-import axios from "axios";
-const fs = require("fs");
+import {Command, flags} from '@oclif/command'
+import axios from 'axios'
+const fs = require('fs')
 
 class NewBlogPost extends Command {
-  static description = "create a basic post template file.";
+  static description = 'create a basic post template file.';
 
   static flags = {
     // add --version flag to show CLI version
-    version: flags.version({ char: "v" }),
-    help: flags.help({ char: "h" }),
+    version: flags.version({char: 'v'}),
+    help: flags.help({char: 'h'}),
     keyword: flags.string({
-      char: "k",
-      description: "auto get a image url about the keyword.",
+      char: 'k',
+      description: 'auto get a image url about the keyword.',
+    }),
+    debug: flags.boolean({
+      char: 'd',
+      description: 'debug mode, print more infos',
+      default: false,
     }),
   };
 
-  static args = [{ name: "title", required: true }];
+  static args = [{name: 'title', required: true}];
 
   async run() {
-    const { args, flags } = this.parse(NewBlogPost);
-    const keyword = flags.keyword ?? "code";
-    const basicUrl = `https://api.unsplash.com/photos/random?client_id=${process.env.UNSPLASH_ID}&query=${keyword}`;
-    const title = args.title;
-    const date = new Date().toLocaleDateString();
-    const fileName = title.replace(/ /g, "-");
+    const {args, flags} = this.parse(NewBlogPost)
+    const keyword = flags.keyword ?? 'code'
+    const debug = flags.debug
+    const basicUrl = `https://api.unsplash.com/photos/random?client_id=${process.env.UNPLASH_ID}&query=${keyword}`
+    const title = args.title
+    const date = new Date().toLocaleDateString()
+    const fileName = title.replace(/ /g, '-')
     axios({
-      method: "GET",
+      method: 'GET',
       url: basicUrl,
+    }).then(res => {
+      if (res.status === 200) {
+        const coverImg = res.data.urls.small
+        const mainImg = res.data.urls.regular
+        const content = `---\ntitle: '${title}'\ndate: '${date}'\ntags:\n- \nmainImg: '${mainImg}'\ncoverImg: '${coverImg}'\nintro: ''\n---\n\n`
+        fs.writeFileSync(`${fileName}.md`, content, 'utf8')
+      } else if (debug) {
+        console.log(res.data)
+      }
     })
-      .then((res) => {
-        if (res.status !== 200) {
-          console.log("unsplash access key 已失效.");
-        } else {
-          const coverImg = res.data.urls.small;
-          const mainImg = res.data.urls.regular;
-          const content = `---\ntitle: '${title}'\ndate: '${date}'\ntags:\n- \nmainImg: '${mainImg}'\ncoverImg: '${coverImg}'\nintro: ''\n---\n\n`;
-          fs.writeFileSync(`${fileName}.md`, content, "utf8");
-        }
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
+    .catch((error: Error) => {
+      if (debug) {
+        console.log(`request url is: ${basicUrl}
+        error message is ${error.message}`)
+      } else {
+        console.log('响应失败,请重试.')
+      }
+    })
   }
 }
 
